eabdf55a5801cc75a0ffbe427e41e72c5ae955d9
Daniel Leite de Oliveira <dleitee@gmail.com>
2015-10-23 14:25:59 -0200

    linter

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,31 +1,31 @@
-console.time('require');
-import gulp from 'gulp';
-import stylus from 'gulp-stylus';
-import uglify from 'gulp-uglify';
-import imagemin from 'gulp-imagemin';
-console.timeEnd('require');
+console.time('require')
+import gulp from 'gulp'
+import stylus from 'gulp-stylus'
+import uglify from 'gulp-uglify'
+import imagemin from 'gulp-imagemin'
+console.timeEnd('require')
 
 gulp.task('css', () => {
   return gulp.src('./src/css/*.styl')
     .pipe(stylus({
       compress: true
     }))
-    .pipe(gulp.dest('./build/css'));
-});
+    .pipe(gulp.dest('./build/css'))
+})
 
 gulp.task('js', () => {
   return gulp.src('./src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
-});
+    .pipe(gulp.dest('./build/js'))
+})
 
 gulp.task('img', () => {
   return gulp.src('./src/img/*')
     .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}]
     }))
-    .pipe(gulp.dest('./build/img'));
-});
+    .pipe(gulp.dest('./build/img'))
+})
 
-gulp.task('default', ['css', 'js', 'img'], () => {} );
+gulp.task('default', ['css', 'js', 'img'], () => {})
--- src/js/Teste.js
+++ src/js/Teste.js
@@ -1,4 +1,4 @@
-(function(){
-    'use strict';
-    console.log('teste');
-}());
+(function () {
+  'use strict'
+  console.log('teste')
+})()
